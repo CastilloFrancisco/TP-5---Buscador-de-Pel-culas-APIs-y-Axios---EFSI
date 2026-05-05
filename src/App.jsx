a7f254932fb6483bf77041983299177ce94dbb32
FRANCISCO CASTILLO <49437690@ort>
2026-05-05 11:31:33 -0300

    appJsx terminado, falta agregar los componentes

--- src/App.jsx
+++ src/App.jsx
@@ -1,122 +1,111 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
+import { useEffect, useState } from 'react'
+
+import axios from "axios";
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [lista, setLista] = useState([]);
+  const [error, setError] = useState("");
+  const [peliculaGrande, setPelicula] = useState("")
+  useEffect(() => { obtenerListaBase(); }, []);
+
+  const buscarPelícula = async (texto) => {
+
+    if (!imdbID) {
+      setError("Ingresá una película");
+      return;
+    }
+
+    try {
+      setError("");
+
+      const response = await axios.get(
+        `http://www.omdbapi.com/?t=${texto}`
+      );
+
+      setLista([response.data]);
+
+    } catch (err) {
+
+      setError("No existe esa película :(");
+      setLista([]);
+
+    }
+  };
+
+  const obtenerLista = async (texto) => {
+
+    setError("");
+
+    try {
+      const response = await axios.get(
+        `http://www.omdbapi.com/?apikey=e84efcd2&s=${texto}&page=1`
+      );
+
+      const detalles = await Promise.all(
+        response.data.Search.map(async (p) => {
+          const res = await axios.get(
+            `http://www.omdbapi.com/?apikey=e84efcd2&i=${p.imdbID}`
+          );
+          return res.data;
+        })
+      );
+
+      setLista(detalles.filter((p) => p));
+    } catch {
+      setError("Error cargando lista");
+    }
+  };
+
+  const obtenerListaBase = async () => {
+    const movieIds = [
+      "tt5726616", // CMBYN
+      "tt0147800", // 10 cosas que odio de vos
+      "tt3783958", // La La Land
+      "tt0074119", // All the Presidents Men
+      "tt0388795", // Secreto en la Montaña
+      "tt2980516", // La teoría del Todo
+      "tt0119217", // Good Will Hunting
+      "tt4574334", // Stranger Things
+      "tt4975722", // Moonlight
+      "tt4034228", // Manchester by the sea
+      "tt21192142", // All of us Strangers
+      "tt0480249", // Soy leyenda
+      "tt23867882", // Pokemon
+      "tt0371746", // Ironman
+      "tt1442556", // Gaturro
+      "tt21436120", // Gaturro serie
+      "tt0952640", // Alvin y las ardillas
+      "tt1615918", // Alvin y las ardillas 3
+      "tt0412142", // Dr House en honor a Bruno
+      "tt1329164"  // Algo de Dr House en honor a Bruno tmb
+    ];
+
+    const shuffled = [...movieIds].sort(() => Math.random() - 0.5);
+    const seleccionados = shuffled.slice(0, 10);
+
+    const detalles = await Promise.all(
+      seleccionados.map(async (id) => {
+        const res = await axios.get(
+          `http://www.omdbapi.com/?apikey=e84efcd2&i=${id}`
+        );
+        return res.data;
+      })
+    );
+
+    setLista(detalles);
+  };
 
   return (
     <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          type="button"
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
+      <Buscador buscarPelicula={buscarPelícula} />
 
-      <div className="ticks"></div>
+      {error && <Error mensaje={error} />}
+      <PeliculaGrande p={peliculaGrande} />
 
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
-
-      <div className="ticks"></div>
-      <section id="spacer"></section>
+      <ListaPeliculas lista={lista} />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
